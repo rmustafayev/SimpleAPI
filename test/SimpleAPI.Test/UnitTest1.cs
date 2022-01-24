8ea14b086250e2c6a668b43ad89a4f96c5ae30a9
Ramil Mustafayev <r.mustafayev@mail.com>
2022-01-24 16:50:40 +0300

    Initial commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+
+        WeatherForecastController controller = new WeatherForecastController();
+        [Fact]
+        public void GetReturnsMyName()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Ramil Mustafayev",returnValue.Value);
+        }
+
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
